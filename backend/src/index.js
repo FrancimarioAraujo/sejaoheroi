6853fed6461297e068601bea4a4fb3a4c539dc69
FrancimarioAraujo <francimariofilho7@gmail.com>
2020-03-25 14:32:10 -0300

    front-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,8 @@ const routes = require('./routes');
 const app = express();
 const cors = require('cors');
 app.use(express.json());
-app.use(routes);
 app.use(cors());
+app.use(routes);
+
 
 app.listen(3333);
